659ca830e7694030674568e9d6f7270f2b9e085b
jimit200 <jimit.gosar200@gmail.com>
2018-10-29 09:34:30 -0500

    added class notes

--- Chapter 6 Notes/Notes.java
+++ Chapter 6 Notes/Notes.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Write a description of class Notes here.
  *
@@ -114,19 +116,134 @@ public class Notes
         
         System.out.println("done");            
     }
-    public static void doExample ()
-    {
-    /*
-     * do loop (do while loop):
-     * 1. exectutes the body of the loop
-     * 2. evaluates the conditional expression
-     * if true, executes the conditional expression 
-     * if true executes the body of the loop
-     */
-    int count = 1;//initialization
     
+    public static void doExample()
+    {
+        /*
+         * do loop (do-while loop):
+         * 
+         *  1. executes the body of the loop
+         *  2. evaluates the conditional expression
+         *      if true, executes the body of the loop again
+         *      if false, continues execution after the loop
+         */
+        int count = 1;                  // initialization
+        
+        do
+        {
+            System.out.println(count);  // body
+            
+            count++;                    // update the condition var
+        }
+        while(count <= 5);              // condition
+        
+        System.out.println("done");
+    }
+    
+    public static int sum()
+    {
+        Scanner s = new Scanner(System.in);
+        
+        int sum = 0;
+        int value;
+        
+        do
+        {
+            System.out.print("enter a positive integer (-1 to quit: ");
+            
+            /*
+             * Sentinel Value
+             *      value (e.g., -1) used to terminate a loop
+             *      it is often entered by a user
+             */
+            value = s.nextInt();
+            
+            // this is a bug! sum is 1 less than it should be
+            sum += value;
+        }
+        while(value != -1);
+        
+        return sum;
+    }
+    
+    public static int sum2()
+    {
+        Scanner s = new Scanner(System.in);
+        
+        int sum = 0;
+        int value;
+        
+        do
+        {
+            System.out.print("enter a positive integer (-1 to quit): ");
+            value = s.nextInt();
+            
+            if(value == -1)
+            {
+                /*
+                 * break: immediately exits the inner-most loop
+                 *      it is in (similar behavior as used in
+                 *      switch statements)
+                 */
+                break;
+            }
+            
+            sum += value;
+        }
+        while(value != -1);
+        
+        return sum;
+    }
+    
+    public static int sum3()
+    {
+        Scanner s = new Scanner(System.in);
+        
+        int sum = 0;
+        int value = 0;
+        
+        do
+        {
+            System.out.print("enter a positive integer (-1 to quit): ");
+            
+            /*
+             * The hasNextInt method of the Scanner class returns
+             *      true if the next token to be read is an integer;
+             *      otherwise, it returns false. It does not consume
+             *      the next token.
+             */
+            if(s.hasNextInt() == false)
+            {
+                System.out.println(s.next() +
+                        " is not an integer; try again.");
+                /*
+                 * continue:
+                 *      1. immediately skips to the end of the
+                 *          inner-most loop it is in
+                 *      2. re-evaluates the loop condition at that
+                 *          point and continues if the condition is
+                 *          true
+                 */        
+                continue;
+            }
+            
+            value = s.nextInt();
+            
+            if(value == -1)
+            {
+                break;
+            }
+            
+            sum += value;
+        }
+        while(value != -1);
+        
+        return sum;
     }
     
     
 }
+
+
+
 
